62d2948d86ba722f44f5c2e3d71975885cb02f49
Kevin Mulugu <kevinmulugu@gmail.com>
2023-01-26 16:00:43 +0300

    fix: lame name

--- src/Providers/QebolPasswordPolicyServiceProvider.php
+++ src/Providers/QebolPasswordPolicyServiceProvider.php
@@ -13,9 +13,9 @@ class QebolPasswordPolicyServiceProvider extends ServiceProvider
         $this->commands([
             CheckPasswordPolicyExpiry::class
         ]);
-        
+
         $this->publishes([
-            __DIR__.'/../../database/migrations/2023_01_26_101909_create_password_policy_table.php' => database_path('migrations/create_permission_tables.php'),
+            __DIR__.'/../../database/migrations/2023_01_26_101909_create_password_policy_table.php' => database_path('migrations/create_password_policy_table.php'),
             __DIR__.'/../../database/migrations/2023_01_26_101926_create_password_changes_table.php' => database_path('migrations/create_password_changes_table.php'),
             __DIR__.'/../../database/migrations/2023_01_26_122505_add_policy_status_to_users_table.php' => database_path('migrations/add_policy_status_to_users_table.php'),
         ], 'password-policy-migrations');
