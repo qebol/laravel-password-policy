defa10f6809cf92a6df4e610898ad33182973f12
Kevin Mulugu <kevinmulugu@gmail.com>
2023-01-26 15:10:52 +0300

    update: fix migrations path

--- src/Providers/QebolPasswordPolicyServiceProvider.php
+++ src/Providers/QebolPasswordPolicyServiceProvider.php
@@ -10,8 +10,8 @@ class QebolPasswordPolicyServiceProvider extends ServiceProvider
     public function register()
     {
         $this->publishes([
-            __DIR__.'/../../database/migrations/2023_01_26_101909_create_password_policy_table.php' => $this->getMigrationFileName('create_permission_tables.php'),
-            __DIR__.'/../../database/migrations/2023_01_26_101926_create_password_changes_table.php' => $this->getMigrationFileName('create_password_changes_table.php'),
+            __DIR__.'/../../database/migrations/2023_01_26_101909_create_password_policy_table.php' => database_path('migrations/create_permission_tables.php'),
+            __DIR__.'/../../database/migrations/2023_01_26_101926_create_password_changes_table.php' => database_path('migrations/create_password_changes_table.php'),
         ], 'password-policy-migrations');
 
     }
